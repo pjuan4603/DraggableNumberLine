870eb8c60bc83e2c795c6cc5e07326b4fd62ea82
Brian Juan <43415804+pjuan4603@users.noreply.github.com>
2023-08-30 19:49:16 -0400

    Code clean up on js

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const point2 = document.getElementById('point2');
 const highlight = document.getElementById('highlight');
 const leftPointer = document.getElementById('leftPointer');
 const rightPointer = document.getElementById('rightPointer');
-const percentage_vw = [20, 23, 28.4, 33.8, 39.2, 44.6, 50, 55.4, 60.8, 66.2, 71.6, 77, 80]; //vw
+const percentage_vw = [20, 23, 28.4, 33.8, 39.2, 44.6, 50, 55.4, 60.8, 66.2, 71.6, 77, 80];
 const num_labels = [-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6]
 
-var leftPointerValue = 0
-var rightPointerValue = 0
 var value = [0, 0]
 
 let draggingPoint = null;
@@ -33,7 +31,6 @@ document.addEventListener('mousemove', (e) => {
   highlight.style.width = Math.abs(parseFloat(point2.style.left) - parseFloat(point1.style.left)) + '%';
 
   var pointerValue = num_labels[percentage_vw.indexOf(closestTick)];
-  console.log(pointerValue);
 
   if (draggingPoint === point1) {
     value[0] = pointerValue;
